5c449e557eb6c568d072e7b58d67ab90581eb039
liang1861321 <1773569548@qq.com>
2019-07-30 08:42:22 +0800

    登录返回值修改

--- aigou-plat-parent/aigou-plat-service/src/main/java/cn/itsource/plat/controller/UserController.java
+++ aigou-plat-parent/aigou-plat-service/src/main/java/cn/itsource/plat/controller/UserController.java
@@ -3,17 +3,20 @@ package cn.itsource.plat.controller;
 import cn.itsource.basic.util.AjaxResult;
 import cn.itsource.plat.domain.User;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
     @PostMapping("/login")
-    public AjaxResult login(String username,String password){
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setId(18L);
-        return AjaxResult.me().setSuccess(true).setMessage("登录成功").setRestObj(user);
+    public AjaxResult login(@RequestBody User user){
+        String username = user.getUsername();
+        String password = user.getPassword();
+        //假登录
+        if("admin".equals(username)&&"admin".equals(password)){
+            return AjaxResult.me().setSuccess(true).setMessage("登录成功!").setRestObj(user);
+        }
+        return AjaxResult.me().setSuccess(false).setMessage("登录失败!");
     }
 }
